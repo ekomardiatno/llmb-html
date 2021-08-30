dc13fe7b9b90f3f89de367c23fbd39a5177da5c5
Eko Mardiatno <ekomardiatno@gmail.com>
2021-08-30 23:20:50 +0700

    Signed-off-by: Eko Mardiatno <ekomardiatno@gmail.com>

--- src/js/affix.js
+++ src/js/affix.js
@@ -13,6 +13,23 @@
  *            twitter.com/ekomardiatno           *
  * ============================================= */
 
-if (typeof jQuery === 'undefined') {
-  throw new Error('Koma Plugin\'s JavaScript requires jQuery')
-}
++function ($) {
+  'use strict'
+
+  $.fn.affix = function (props) {
+    var $this = this
+    return $this.each(function () {
+      $(window).on('scroll', function (e) {
+        if (e.currentTarget.scrollY >= props.top) {
+          $this.addClass('affix')
+        } else {
+          $this.removeClass('affix')
+        }
+      })
+    })
+  }
+}(jQuery)
+
+$('nav.scrlspy-affix').affix({
+  top: $('nav.scrlspy-affix').attr('data-top-target') ? parseInt($('nav.scrlspy-affix').attr('data-top-target')) : 50
+})
